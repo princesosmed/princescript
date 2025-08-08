4600175be9a9c3330a4019b335b7f88bf8027ab7
princesosmed <yasircheatenak@gmail.com>
2025-08-08 07:43:58 +0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,3 @@
+-- Script Gabungan
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/numerouno2/eugunewuhub/refs/heads/main/eugunewustd.lua"))()
